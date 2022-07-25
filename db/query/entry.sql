278d95acff5f12deb84278eaf4c51cc13a331859
kashshof <alisher.abdusattorov@gmail.com>
2022-07-25 22:15:43 +0500

    initial commit

---
+++ db/query/entry.sql
@@ -0,0 +1,18 @@
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+ORDER BY id;
+
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id, amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE id = $1;
